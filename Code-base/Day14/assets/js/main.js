e11bab7aa5017198269682f005a31127b94ed1bf
Vishal Jain <jainvishal628@gmail.com>
2021-08-04 20:09:00 +0530

    Day14 Boiler Plate

---
+++ Code-base/Day14/assets/js/main.js
@@ -0,0 +1,31 @@
+
+/*
+
+    ' b = undefined                           '
+    ' c = undefined                           '
+    ' addFontBoldClass =  {                   '
+    '        var class_name = "font-bold";    '
+    '       return class_name;                '
+    '    }                                    '
+    '-----------------------------------------'
+    Memory Space / Global Space
+
+    ' b = undefined                           '
+    ' c = undefined                           '
+    ' addFontBoldClass =  {                   '
+    '        var class_name = "font-bold";    '
+    '       return class_name;                '
+    '    }                                    '
+    '-----------------------------------------'
+    Content Execution Space
+    
+  var a = document.getElementById('demo').classList; //statement
+        
+    everything inside a {} is a block
+
+  == will check only the Value of the variable NOT THE DATA TYPE
+    number + number = number
+    string + string/number/boolean = string
+    js programs is collection of statements.
+
+*/
