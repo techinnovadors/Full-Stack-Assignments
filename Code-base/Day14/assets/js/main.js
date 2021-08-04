eb496297dab0df1d904081f249088e88c7c43a42
Vishal Jain <jainvishal628@gmail.com>
2021-08-04 21:33:13 +0530

    Calculator Completed

--- Code-base/Day14/assets/js/main.js
+++ Code-base/Day14/assets/js/main.js
@@ -1,6 +1,6 @@
-
 /*
 
+    HOISTING
     ' b = undefined                           '
     ' c = undefined                           '
     ' addFontBoldClass =  {                   '
@@ -29,3 +29,41 @@
     js programs is collection of statements.
 
 */
+
+/* 
+   INSERT   
+   CLEAR
+   BACKSPACE   // it will remove the last character from the input  
+   in Java/C++ we have substring
+
+   equal
+*/
+
+var expression = document.getElementById("result");
+
+function insert(val) {
+  expression.value += val;
+}
+
+function backspace() {
+  expression.value = expression.value.slice(0, -1);
+}
+
+function clear() {
+  expression.value = "";
+}
+
+function equal() {
+  console.log(expression.value);
+  expression.value = eval(expression.value);
+}
+
+/**
+ * Eval input is string
+ * 
+ * 
+ * Processing it will convert the String into Javascript Code
+ * 
+ * and then it will execute.
+ * 
+ * **/
