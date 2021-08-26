e35a4901d1476d1b42c6be75eaf86dbc1379dbec
Vishal Jain <jainvishal628@gmail.com>
2021-08-26 22:47:53 +0530

    Day23_4

--- Code-base/Day23/routes/authorRoutor.js
+++ Code-base/Day23/routes/authorRoutor.js
@@ -31,7 +31,125 @@ router.post('/', async (req, res) => {
 })
 
 
+router.get('/', async (req, res) => {
+    try {
+        const authors = await authorModel.find({});
+        res.json({
+            data: authors,
+            message: "Successfull"
+        });
+    } catch (error) {
+        res.json({
+            data: [],
+            message: error
+        });
+    }
+})
 
+router.get('/:author_id', async (req, res) => {
+    const {
+        author_id
+    } = req.params
+    try {
+        const authors = await authorModel.find({
+            "id": author_id
+        }, '-_id -__v');
+        res.json({
+            data: authors,
+            message: "Successfull"
+        });
+    } catch (error) {
+        res.json({
+            data: [],
+            message: error
+        });
+    }
+})
+
+router.get('/getName/:author_id', async (req, res) => {
+    const {
+        author_id
+    } = req.params
+    try {
+        const authors = await authorModel.find({
+            "id": author_id
+        }, 'name');
+        res.json({
+            data: authors,
+            message: "Successfull"
+        });
+    } catch (error) {
+        res.json({
+            data: [],
+            message: error
+        });
+    }
+});
+
+
+router.put('/updateName', async (req, res) => {
+
+    const {
+        author_name,
+        author_id
+    } = req.body
+    try {
+        const authors = await authorModel.updateMany({
+            "id": author_id
+        }, {
+            "name": author_name
+        });
+        res.json({
+            data: authors,
+            message: "Successfull"
+        });
+    } catch (error) {
+        res.json({
+            data: [],
+            message: error
+        });
+    }
+});
+
+
+router.delete('/:author_id', async (req, res) => {
+    const {
+        author_id
+    } = req.params
+    try {
+        const authors = await authorModel.deleteOne({
+            "id": author_id
+        });
+        res.json({
+            data: authors,
+            message: "Successfull"
+        });
+    } catch (error) {
+        console.log(error)
+        res.json({
+            data: [],
+            message: error
+        });
+    }
+})
+
+
+router.delete('/', async (req, res) => {
+
+    try {
+        const authors = await authorModel.deleteMany({});
+        res.json({
+            data: authors,
+            message: "Successfull"
+        });
+    } catch (error) {
+        console.log(error)
+        res.json({
+            data: [],
+            message: error
+        });
+    }
+})
 
 
 module.exports = router;
